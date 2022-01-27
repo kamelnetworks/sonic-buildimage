44328a76e3feaae828f4b21e0b2ea8b54aae1424
Christian Svensson <blue@cmd.nu>
2022-01-27 22:40:17 +0100

    kamel: caclmgrd: Tests for ip2me block
    Signed-off-by: Christian Svensson <blue@cmd.nu>

--- src/sonic-host-services/tests/caclmgrd/caclmgrd_ip2me_test.py
+++ src/sonic-host-services/tests/caclmgrd/caclmgrd_ip2me_test.py
@@ -7,16 +7,16 @@ from sonic_py_common.general import load_module_from_source
 from unittest import TestCase, mock
 from pyfakefs.fake_filesystem_unittest import patchfs
 
-from .test_dhcp_vectors import CACLMGRD_DHCP_TEST_VECTOR
+from .test_ip2me_vectors import CACLMGRD_IP2ME_TEST_VECTOR
 from tests.common.mock_configdb import MockConfigDb
 
 
 DBCONFIG_PATH = '/var/run/redis/sonic-db/database_config.json'
 
 
-class TestCaclmgrdDhcp(TestCase):
+class TestCaclmgrdIP2Me(TestCase):
     """
-        Test caclmgrd dhcp
+        Test caclmgrd IP2Me
     """
     def setUp(self):
         swsscommon.ConfigDBConnector = MockConfigDb
@@ -27,29 +27,15 @@ class TestCaclmgrdDhcp(TestCase):
         caclmgrd_path = os.path.join(scripts_path, 'caclmgrd')
         self.caclmgrd = load_module_from_source('caclmgrd', caclmgrd_path)
 
-    @parameterized.expand(CACLMGRD_DHCP_TEST_VECTOR)
+    @parameterized.expand(CACLMGRD_IP2ME_TEST_VECTOR)
     @patchfs
-    def test_caclmgrd_dhcp(self, test_name, test_data, fs):
+    def test_caclmgrd_ip2me(self, test_name, test_data, fs):
         if not os.path.exists(DBCONFIG_PATH):
             fs.create_file(DBCONFIG_PATH) # fake database_config.json
 
         MockConfigDb.set_config_db(test_data["config_db"])
-
-        with mock.patch("caclmgrd.subprocess") as mocked_subprocess:
-            popen_mock = mock.Mock()
-            popen_attrs = test_data["popen_attributes"]
-            popen_mock.configure_mock(**popen_attrs)
-            mocked_subprocess.Popen.return_value = popen_mock
-
-            call_rc = test_data["call_rc"]
-            mocked_subprocess.call.return_value = call_rc
-
-            mark = test_data["mark"]
-
-            caclmgrd_daemon = self.caclmgrd.ControlPlaneAclManager("caclmgrd")
-            mux_update = test_data["mux_update"]
-
-            for key,data in mux_update:
-                caclmgrd_daemon.update_dhcp_acl(key, '', data, mark)
-
-            mocked_subprocess.call.assert_has_calls(test_data["expected_subprocess_calls"], any_order=False)
+        self.caclmgrd.ControlPlaneAclManager.get_namespace_mgmt_ip = mock.MagicMock()
+        self.caclmgrd.ControlPlaneAclManager.get_namespace_mgmt_ipv6 = mock.MagicMock()
+        caclmgrd_daemon = self.caclmgrd.ControlPlaneAclManager("caclmgrd")
+        ret = caclmgrd_daemon.generate_block_ip2me_traffic_iptables_commands('')
+        self.assertEqual(ret, test_data["return"])
